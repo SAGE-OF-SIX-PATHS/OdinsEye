218218a8b65258581bdb8ab5a5fb052a3004b178
Ken-zzy <nwakalokenechukwu@gmail.com>
2025-04-12 00:52:15 +0100

    April 12, 2025 12:52 AM

--- backend/src/routes/factCheck.route.ts
+++ backend/src/routes/factCheck.route.ts
@@ -1,9 +1,9 @@
-
 import { Router } from "express";
 import { factCheck } from "../controllers/factCheck.controller";
 
 const router = Router();
 
+// POST /fact-check
 router.post("/", factCheck);
 
 export default router;
